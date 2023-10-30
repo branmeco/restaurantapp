78a4c422956da1cd2f7ff85a398befb3a15ee6b5
branmeco <branmeco17@gmail.com>
2023-10-30 08:21:18 -0500

    Tailwind Basics

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Notification() {
   return (
-    <div>Notification</div>
+    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>Free delivery for all orders over $50. Order your food now!</div>
   )
 }
 
