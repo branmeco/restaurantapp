5ec0d83878bef3fce3481fdf310d35d106cb1607
branmeco <branmeco17@gmail.com>
2023-10-30 11:24:35 -0500

    Footer Design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
+      <Link href="/">MASSIMO</Link>
+      <p>¢ ALL RIGHTS RESERVER</p>
+    </div>
   )
 }
 
