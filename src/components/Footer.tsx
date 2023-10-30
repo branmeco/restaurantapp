cfe51d06879d089a1904419c56629e0d20674c26
branmeco <branmeco17@gmail.com>
2023-10-30 15:49:48 -0500

    React Tailwind Responsive Slider Tutorial

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 function Footer() {
   return (
     <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
-      <Link href="/">MASSIMO</Link>
+      <Link href="/" className=''>MASSIMO</Link>
       <p>¢ ALL RIGHTS RESERVER</p>
     </div>
   )
