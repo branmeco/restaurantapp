6019e8e51b25df5294a0cb2837257ae1f69704d7
branmeco <branmeco17@gmail.com>
2023-11-02 15:01:43 -0500

    Orders Page Design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
+    <div className='h-12 md:h-24 p-4 lg:px-20 xl:p-40 text-red-500 flex items-center justify-between'>
       <Link href="/" className=''>MASSIMO</Link>
       <p>¢ ALL RIGHTS RESERVER</p>
     </div>
