f4d21f3cf570f023ffef09d3335aac89793a88b6
branmeco <branmeco17@gmail.com>
2023-11-22 14:17:15 -0500

    Delete Product as an Admin

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { prisma } from "@/utils/connect";
+import { getAuthSession } from '@/utils/auth';
 
 //GET SINGLE PRODUCT
 export const GET = async (
@@ -21,8 +22,43 @@ export const GET = async (
     } catch (err) {
         console.log(err);
         return new NextResponse(
-            JSON.stringify({message: "Something went wrong!"}),
-            {status: 500}
+            JSON.stringify({ message: "Something went wrong!" }),
+            { status: 500 }
         )
     }
+}
+
+//DELETE SINGLE PRODUCT
+export const DELETE = async (
+    req: NextRequest,
+    { params }: { params: { id: string } }
+) => {
+
+    const { id } = params;
+    const session = await getAuthSession()
+
+    if (session?.user.isAdmin) {
+
+        try {
+            await prisma.product.delete({
+                where: {
+                    id: id,
+                },
+            });
+            return new NextResponse(
+                JSON.stringify("Product has been deleted!"),
+                { status: 200 }
+            );
+        } catch (err) {
+            console.log(err);
+            return new NextResponse(
+                JSON.stringify({ message: "Something went wrong!" }),
+                { status: 500 }
+            )
+        }
+    }
+    return new NextResponse(
+        JSON.stringify({ message: "You are not allowed!" }),
+        { status: 403 }
+    )
 }
